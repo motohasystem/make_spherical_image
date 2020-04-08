f247cea109c26eaf1115d9bc5f1e45c8efdf027a
Daisuke Motohashi <ddssk.m@gmail.com>
2020-04-08 22:51:21 +0900

    透過でぼかしをかける

--- make_spherical_image.py
+++ make_spherical_image.py
@@ -6,14 +6,25 @@ def make_spherical(filename):
     width, height = img.size
 
     new_height = int(width / 2)
-    result = Image.new(img.mode, (width, new_height), (0, 0, 0))
+    result = Image.new('RGBA', (width, new_height), (0, 0, 0))
     result.paste(img, (0, new_height - height))
     
-    draw = ImageDraw.Draw(result)
-    color_white = (255, 255, 255)
-    draw.rectangle((0, 0, width, width*0.14), fill=color_white, outline=color_white)
+    blur = Image.new('RGBA', result.size)
+    
+    draw = ImageDraw.Draw(blur)
+    bottom = width*0.14
+
+    start = 0
+    for i in range(255):
+        end = i
+        draw.rectangle((0, bottom+start, width, bottom+end), fill=(255, 255, 255, 255-i))
+        start = end
+    
+    draw.rectangle((0, 0, width, bottom), fill=(255, 255, 255, 255))
+    result = Image.alpha_composite(result,blur)
     
     return result
+
 
 
 filename = sys.argv[1]
